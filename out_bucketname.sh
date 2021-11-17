3ec6f16ff5e9f9acffb1a5049b4639f42e9240ec
Lucian Lazar <lucian@mainquest.ro>
2021-11-17 17:27:53 +0200

    removed || exit <code> at the end of lines as the calling script already sets -e flag for bash

--- out_bucketname.sh
+++ out_bucketname.sh
@@ -3,9 +3,9 @@
 b=$1
 if [ -z "$b" ]; then
 	repo="$GITHUB_REPOSITORY"
-	b_no_slashes=$(echo "$repo" | sed "s/\//_/g") || exit 1
-	b_no_dots=$(echo "$b_no_slashes" | sed "s/\./-/g") || exit 2
-	b_lowercase=$(echo "$b_no_dots" | (tr '[:upper:]' '[:lower:]')) || exit 3
+	b_no_slashes=$(echo "$repo" | sed "s/\//_/g")
+	b_no_dots=$(echo "$b_no_slashes" | sed "s/\./-/g")
+	b_lowercase=$(echo "$b_no_dots" | (tr '[:upper:]' '[:lower:]'))
 	b=gh-runs_"$b_lowercase"
 	echo "Using $b as implicit bucket name"
 fi
